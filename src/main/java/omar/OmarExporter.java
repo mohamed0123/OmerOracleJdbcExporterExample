b38c7276b4f807fa9097f770f0a0d4b4bed8340a
unknown <wolfneedlove2006@gmail.com>
2020-06-28 13:53:25 +0200

    add send email

--- src/main/java/omar/OmarExporter.java
+++ src/main/java/omar/OmarExporter.java
@@ -4,9 +4,11 @@ import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.StringJoiner;
 
@@ -17,7 +19,9 @@ class OmarExporter {
 
 		if (args.length == 0)
 			// for test
-			args = new String[] { "./tmp/test.txt", "select * from html_extraction_input where rownum < 5" };
+			args = new String[] { "./tmp/test2.txt", " SELECT\r\n" + 
+					"        *\r\n" + 
+					"    FROM SYS.all_objects where object_type = 'FUNCTION' and owner = 'PDF_EXT'" };
 
 		if (args.length == 2) {
 			filePath = args[0];
@@ -41,6 +45,8 @@ class OmarExporter {
 				Files.write(Paths.get(filePath), createRow(rs).getBytes(), StandardOpenOption.APPEND);
 				System.out.println("current row >>> " + ++counter);
 			}
+			
+			sendEmail(con,"<h1 style=\"color: #5e9ca0;\">Dears,</h1> <p>Report Executed Successfully</p> <h2 style=\"color: #2e6c80;\">Best Regards</h2> <h3 style=\"color: #2e6c80;\">  Omar Seliem </h3>");
 			System.out.println("<<<<<<<<<<<<<<<<<<<< Done >>>>>>>>>>>>>>>>>>>>");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -53,6 +59,16 @@ class OmarExporter {
 
 		}
 
+	}
+	
+	private static void sendEmail(Connection con, String body ) throws Exception {
+		String query = "{call automation2.html_email('omar_seliem@siliconexpert.com', "
+				+ "'mohamed_hamada@siliconexpert.com', 'OMER SELIEM REPORTS <Ba-SW@siliconexpert.com>'    , "
+				+ "'OMER SELIEM REPORTS TITLE', "+(body.length() + 10 )+",?)}"; 
+		CallableStatement statement = con.prepareCall(query);  
+		statement.setString(1, body);  
+		statement.execute();
+		System.out.println("email sended");
 	}
 
 	private static String createRow(ResultSet rs) throws Exception {
